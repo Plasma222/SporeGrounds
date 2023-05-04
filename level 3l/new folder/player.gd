967d1ce0949228002f0da59640cc1683395c5732
Tcbwastkn <125571834+Tcbwastkn@users.noreply.github.com>
2023-05-04 08:45:05 -0400

    Add files via upload

---
+++ level 3l/new folder/player.gd
@@ -0,0 +1,21 @@
+extends KinematicBody2D
+
+var _velocity: Vector2
+var speed := 500.0
+
+
+func _physics_process(delta: float) -> void:
+	var input := Vector2.ZERO
+	if Input.is_action_pressed("ui_left"):
+		input.x -= 1
+	if Input.is_action_pressed("ui_right"):
+		input.x += 1
+	if Input.is_action_pressed("ui_up"):
+		input.y -= 1
+	if Input.is_action_pressed("ui_down"):
+		input.y += 1
+	
+	input = input.normalized()
+	
+	_velocity = _velocity.linear_interpolate(input *speed, 0.2)
+	_velocity = move_and_slide(_velocity, Vector2.UP)
